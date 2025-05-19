5dca66847e91a03d0ab5dbc866b90ec026d2c5f1
monarad <monajamshidirad.65@gmail.com>
2025-05-19 08:55:04 +0330

    feat: setup React Query with QueryClientProvider and integrate Devtools

--- src/providers/TanstackQueryProvider.jsx
+++ src/providers/TanstackQueryProvider.jsx
@@ -1,11 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // eslint-disable-next-line
 function TanstackQueryProvider({ children }) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
